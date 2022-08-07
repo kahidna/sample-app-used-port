b6175875e4b629d9968ff10d68817f7c2cba767f
kahidna <kahid.na@gmail.com>
2022-08-07 06:52:58 +0000

    add string for print hostname

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ echo ""
 
 cd $APPDIR && echo "var express = require('express');
 var app = express();app.get('/', function (req, res) {
-  res.send('Hello World From Node-$NODENUMBER!');
+  res.send('Hello World From Node-$NODENUMBER! With Host Name = $HOSTNAME');
 });app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });" > app.js
